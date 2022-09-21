7353388067ba42dc8b031272dd601f1afc8f0ca2
MinhNguyen153216 <minhnguyen153216@gmail.com>
2022-09-21 20:05:53 +0700

    commit

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -15,10 +15,23 @@ const onChange = (currentSlide) => {
 
 export default function Index() {
   return (
-    <>
+    <div className="carousel">
       <Carousel afterChange={onChange}>
         <div>
-          <h3 style={contentStyle}>1</h3>
+          <div style={{}}>
+            <div className="item-carousel">
+              <div className="container">
+                <div className="item-left">
+                  <img src="./img/image 4.png" alt="..." />
+                </div>
+                <div className="item-right">
+                  <h1>Product name</h1>
+                  <p>Product description ...</p>
+                  <span className="text-center">Buy now</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
           <h3 style={contentStyle}>2</h3>
@@ -30,6 +43,6 @@ export default function Index() {
           <h3 style={contentStyle}>4</h3>
         </div>
       </Carousel>
-    </>
+    </div>
   );
 }
